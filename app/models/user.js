593d43af542d8bcbc038475bdcff05a0396deb9d
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-10-29 13:58:44 -0500

    add

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    'User',
+    'users',
     {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING
     },
-    {}
+    { underscored: true }
   );
   return User;
 };
--- migrations/migrations/20191025165137-create-user.js
+++ migrations/migrations/20191025165137-create-user.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
+    queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,5 +28,5 @@ module.exports = {
         type: Sequelize.DATE
       }
     }),
-  down: queryInterface => queryInterface.dropTable('Users')
+  down: queryInterface => queryInterface.dropTable('users')
 };
