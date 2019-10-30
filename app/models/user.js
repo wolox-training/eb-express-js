b7a72b24ec22a8b6c2e8bb11374f6fe76d6591f9
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-10-30 11:27:12 -0500

    add unique to email

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,10 @@ module.exports = (sequelize, DataTypes) => {
     {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
       password: DataTypes.STRING
     },
     { underscored: true }
